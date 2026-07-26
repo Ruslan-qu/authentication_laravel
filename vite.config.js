7a81b97e422688ab8a85010f56dec5ba14818f68
qu <quqichbakich@mail.ru>
2026-07-26 20:05:04 +0300

    установка

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import {
+    defineConfig
+} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/passkeys.js',
+            ],
+            refresh: true,
+            optimizedFallbacks: false,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        cors: true,
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
