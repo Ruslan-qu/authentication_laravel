aba177ea6210270f8030696051e29eecb7c748f1
qu <quqichbakich@mail.ru>
2026-09-24 20:59:41 +0300

    AuthorizationTest

--- tests/Feature/Http/Controllers/AuthorizationTest.php
+++ tests/Feature/Http/Controllers/AuthorizationTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
 class AuthorizationTest extends TestCase
@@ -13,9 +13,91 @@ class AuthorizationTest extends TestCase
      */
     public function test_route_login(): void
     {
-        $response = $this->get('/login');
+        $response = $this->get(route('login'));
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * Test route login view.
+     *  
+     */
+    public function test_route_login_view(): void
+    {
+
+        $response = $this->get(route('login'));
+
+        $response->assertViewIs('user.login');
+    }
+
+    /**
+     * Test route login guests.
+     */
+    public function test_route_login_guests(): void
+    {
+
+        $response = $this->post(route('login'));
+
+        $this->assertGuest();
+    }
+
+    /**
+     * Test route authorization user invalid.
+     */
+    public function test_route_authorization_user_invalid(): void
+    {
+
+        $user = [
+            'email' => 'ivan.com',
+            'password' => '',
+        ];
+
+        $response = $this->post(route('login'), $user);
+
+        $response->assertSessionHasErrors(['email', 'password']);
+    }
+
+    /**
+     * Test route authorization user valid.
+     */
+    public function test_route_authorization_user_valid(): void
+    {
+
+        $user = [
+            'email' => 'ivan@example.com',
+            'password' => 'password123',
+        ];
+
+        $response = $this->post(route('login'), $user);
+
+        $response->assertValid();
+    }
+
+    /**
+     * Test route authorization user authenticate session.
+     */
+    public function test_route_authorization_user_authenticate_session(): void
+    {
+
+        User::factory()->verified()->create(
+            [
+                'email' => 'ivan@example.com',
+                'password' => 'password123',
+            ]
+        );
+
+        $data = [
+            'email' => 'ivan@example.com',
+            'password' => 'password123',
+        ];
+
+        $sessionId = Session::getId();
+
+        $response = $this->post(route('login'), $data);
+
+        $newSessionId = $response->getSession()->getId();
+
+        $this->assertNotEquals($sessionId, $newSessionId);
     }
 
     /**
@@ -33,7 +115,7 @@ class AuthorizationTest extends TestCase
      */
     public function test_route_logout(): void
     {
-        
+
         $response = $this->get('/logout');
 
         $response->assertRedirect('/login');
